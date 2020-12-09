d62b01d3e593b246d659c27c8bda86d151dc663e
ApTyPuk <kpackalv@yahoo.com>
2020-12-09 22:25:43 +0200

    Dependency Injection and Inversion of Control

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,9 +9,12 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
+        //Music music = context.getBean("musicBean", Music.class);
 
-        System.out.println(testBean.getName());
+       // MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
         context.close();
 
