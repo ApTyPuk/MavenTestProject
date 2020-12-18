8c45cae2710ad7671c6d82511c159fa7dcd46366
ApTyPuk <kpackalv@yahoo.com>
2020-12-18 12:49:12 +0200

    Spring @Annotations

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -28,6 +28,16 @@ public class TestSpring {
         System.out.println(musicPlayer.playMusic(MusicStyles.CLASSICAL));
         System.out.println(musicPlayer.playMusic(MusicStyles.ROCK));
 
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 =
+                context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 =
+                context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic1 == classicalMusic2);
+
         context.close();
 
     }
